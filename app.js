1514695829d434905e63391db42d827e2d2ca913
Vineet Maurya <101665909+Vineet369@users.noreply.github.com>
2024-09-25 16:34:49 +0530

    updates readme with project overview

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const regionRoutes = require('./routes/region.route');
 const cycleRoutes = require('./routes/cycle.route');
 
 // Middleware
+app.use(express.json({limit: "16kb"}))
+app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(bodyParser.json());
 
 app.use('/api/users', userRoutes);
